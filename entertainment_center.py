09e9ab525a53e9e1b60eb59a079054b60524364f
Basant Singh <basant1@users.noreply.github.com>
2018-04-15 23:32:30 -0400

    Fix styling

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,35 +1,61 @@
 import fresh_tomatoes
 import media
 
-"""Create six Movie instances each with title, storyline, poster image, and YouTube trailer of the class Movie defined in media.py."""
+"""Create six Movie instances each with title, storyline, poster image, and
+YouTube trailer of the class Movie defined in media.py."""
 
-thor_ragnarok = media.Movie("Thor: Ragnarok", 
-                        "Thor is imprisoned on the other side of the universe without his mighty hammer and finds himself in a race against time to get Back to Asgard to stop Ragnarok", "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg", "https://www.youtube.com/watch?v=ue80QwXMRHg")
+thor_ragnarok = media.Movie(
+    "Thor: Ragnarok",
+    "Thor is imprisoned on the other side of the universe without his mighty\
+    hammer and finds himself in a race against time to get Back to Asgard to\
+    stop Ragnarok",
+    "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg",
+    "https://www.youtube.com/watch?v=ue80QwXMRHg")
 
-black_panther = media.Movie("Black Panther",
-                     "After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king",
-                     "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg",
-                     "https://www.youtube.com/watch?v=xjDjIWPwcPU")
+black_panther = media.Movie(
+    "Black Panther",
+    "After the death of his father, T'Challa returns home to the African\
+    nation of Wakanda to take his rightful place as king",
+    "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=xjDjIWPwcPU")
 
-spiderman_homecoming = media.Movie("Spider-Man: Homecoming", 
-                             "Under the watchful eye of mentor Tony Stark, Parker starts to embrace his newfound identity as Spider-Man. Peter must soon put his powers to the test when the evil Vulture emerges to threaten everything that he holds dear.",
-                             "https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg",
-                             "https://www.youtube.com/watch?v=U0D3AOldjMU")
+spiderman_homecoming = media.Movie(
+    "Spider-Man: Homecoming",
+    "Under the watchful eye of mentor Tony Stark, Parker starts to embrace his\
+    newfound identity as Spider-Man. Peter must soon put his powers to the\
+    test when the evil Vulture emerges to threaten everything that he holds\
+    dear.",
+    "https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=U0D3AOldjMU")
 
-doctor_strange = media.Movie("Doctor Strange",
-                          "The story of world-famous neurosurgeon Dr. Stephen Strange whose life changes forever after a horrific car accident robs him of the use of his hands",
-                          "https://upload.wikimedia.org/wikipedia/en/c/c7/Doctor_Strange_poster.jpg","https://www.youtube.com/watch?v=HSzx-zryEgM")
+doctor_strange = media.Movie(
+    "Doctor Strange",
+    "The story of world-famous neurosurgeon Dr. Stephen Strange whose life\
+    changes forever after a horrific car accident robs him of the use of his\
+    hands",
+    "https://upload.wikimedia.org/wikipedia/en/c/c7/Doctor_Strange_poster.jpg",
+    "https://www.youtube.com/watch?v=HSzx-zryEgM")
 
-captain_america_civil_war = media.Movie("Captain America: Civil War",
-                                "The new status quo deeply divides members of the team. Captain America believes superheroes should remain free to defend humanity without government interference. Iron Man sharply disagrees and supports oversight.",
-                                "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",
-                                "https://www.youtube.com/watch?v=dKrVegVI0Us")
+captain_america_civil_war = media.Movie(
+    "Captain America: Civil War",
+    "The new status quo deeply divides members of the team. Captain America\
+    believes superheroes should remain free to defend humanity without\
+    government interference. Iron Man sharply disagrees and supports\
+    oversight.",
+    "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=dKrVegVI0Us")
 
-avengers_infinity_war = media.Movie("Avengers: Infinity War",
-                           "Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos. On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality.",
-                           "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",
-                           "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
+avengers_infinity_war = media.Movie(
+    "Avengers: Infinity War",
+    "Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle\
+    their most powerful enemy yet -- the evil Thanos. On a mission to collect\
+    all six Infinity Stones, Thanos plans to use the artifacts to inflict his\
+    twisted will on reality.",
+    "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
 
 # Add instances to a list
-movies = [thor_ragnarok, black_panther, spiderman_homecoming, doctor_strange, captain_america_civil_war, avengers_infinity_war]
+movies = [
+    thor_ragnarok, black_panther, spiderman_homecoming, doctor_strange,
+    captain_america_civil_war, avengers_infinity_war]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,13 +1,25 @@
 import webbrowser
 
-class Movie():
-    """This class provides a way to store movie related information"""
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+class Movie():
+    """This class provides a way to store movie related information.
+
+    Attributes:
+        title: A string that stores the title of the movie
+        storyline: A string that stores a short description of the movie
+        poster_image_url: A string that stores the url of the movie poster
+        trailer_youtube_url: A string that stores the url of the movie's
+        YouTube trailer
+
+    """
+
+    def __init__(
+                self, movie_title, movie_storyline, poster_image,
+                trailer_youtube):
+                    self.title = movie_title
+                    self.storyline = movie_storyline
+                    self.poster_image_url = poster_image
+                    self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
